7f7232cb997ec67152518d098918044c476464ca
Ivan Towlson <github@hestia.cc>
2015-08-13 13:35:21 +1200

    Code correctness sample

---
+++ Analyser Samples/OperationsMistakenForInPlace/OperationsMistakenForInPlace/OperationsMistakenForInPlace.Test/UnitTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using OperationsMistakenForInPlace;
+
+namespace OperationsMistakenForInPlace.Test
+{
+    [TestClass]
+    public class UnitTest : CodeFixVerifier
+    {
+        [TestMethod]
+        public void IfStringReplaceIsCalledAndResultIsAssigned_ItIsOkay()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class C
+        {
+            public void Huzzah()
+            {
+                string s = ""Hello world"";
+                var t = s.Replace("" "", ""_"");
+                Console.WriteLine(t);
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void IfStringReplaceIsCalledAndResultIsPassedToAnotherMethod_ItIsOkay()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class C
+        {
+            public void Huzzah()
+            {
+                string s = ""Hello world"";
+                Console.WriteLine(s.Replace("" "", ""_""));
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void IfStringReplaceIsCalledAndResultNotAssigned_ItIsDiagnosed()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class C
+        {
+            public void Oops()
+            {
+                string s = ""Hello world"";
+                s.Replace("" "", ""_"");
+                Console.WriteLine(s);
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "OperationsMistakenForInPlace",
+                Message = "Result of 'string.Replace' is discarded - did you mean to assign it?",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 11, 17)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            throw new NotSupportedException();  // we're not implementing a code fix for this one (at least not for now)
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new OperationsMistakenForInPlaceAnalyzer();
+        }
+    }
+}
---
+++ Analyser Samples/OperationsMistakenForInPlace/OperationsMistakenForInPlace/OperationsMistakenForInPlace/DiagnosticAnalyzer.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace OperationsMistakenForInPlace
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class OperationsMistakenForInPlaceAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "OperationsMistakenForInPlace";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.SimpleMemberAccessExpression);
+        }
+
+        private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+        {
+            var accessNode = (MemberAccessExpressionSyntax)(context.Node);
+            var assignment = accessNode.FirstAncestorOrSelf<EqualsValueClauseSyntax>();
+            var args = accessNode.FirstAncestorOrSelf<ArgumentListSyntax>();
+
+            bool resultIsUsed = assignment != null || args != null;
+            if (resultIsUsed)
+            {
+                return;
+            }
+
+            var resolutions = context.SemanticModel.GetMemberGroup(accessNode);
+
+            if (resolutions == null || resolutions.IsDefaultOrEmpty)
+            {
+                return;
+            }
+
+            var member = resolutions[0];  // TODO: this might not be the actual overload - but this will typically not be an issue
+            var memberName = member.Name;
+            var typeName = member.ContainingType.ToDisplayString();
+            var memberQName = typeName + "." + memberName;
+
+            if (memberQName == "string.Replace")
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, accessNode.GetLocation(), memberQName);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
